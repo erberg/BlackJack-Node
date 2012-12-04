7db502901c5ce6d23f3f46eb15b2507c5ede1130
erberg <berg.ericr@gmail.com>
2012-12-04 13:32:40 -0800

    Added/Moved CSS

--- index.php
+++ index.php
@@ -1,99 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        
         <meta charset="utf-8">
         <title>Blackjack Card-Counting Application, by Eric R. Berg</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <!-- Le styles -->
         <link href="/css/bootstrap.css" rel="stylesheet">
-        <style>
-            body {
-                padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-                height:100%;
-                width:100%;
-                background-color:whitesmoke;
-            }
-            .spacer{
-                width:100%;
-                min-height:200px;
-                border:5px solid black;
-                float:left;
-            }
-
-            .clearfix {
-                display: inline-block;
-            }
-            
-            .table-container{
-                margin:auto;
-                width:1000px;
-            }
-            .card-table{ 
-                background-color: whitesmoke;
-                position: absolute;
-                width: 1000px;
-                height: 500px;
-                margin: auto;
-                border: 5px solid red;
-            }
-
-            .left-card-table{
-                width:500px;
-                height:500px;
-                border-radius:250px;
-                font-size:20px;
-                color:#fff;
-                line-height:100px;
-                background:greenyellow;
-                position:absolute;
-            }
-            .right-card-table{
-                width:500px;
-                height:500px;
-                border-radius:250px;
-                font-size:20px;
-                color:#fff;
-                line-height:100px;
-                background:greenyellow;
-                position:absolute;
-                left:500px;
-            }
-            .middle-card-table{
-                width:500px;
-                height:500px;
-                font-size:20px;
-                color:#fff;
-                line-height:100px;
-                background:greenyellow;
-                position:absolute;
-                left:250px;
-            }
-            .position-Dealer{
-                background-size:cover;
-                width:90px;
-                height:125.2px;
-                position:absolute;
-            }
-            .position-Player1{
-                background-size:cover;
-                width:90px;
-                height:125.2px;
-                position:absolute;
-            }
-            .card-container{
-                background-size:cover;
-                width:90px;
-                height:125.2px;
-            }
-        </style>
-
-        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-        <!--[if lt IE 9]>
-          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
+        <link href="/css/blackjack.css" rel="stylesheet">
 
     </head>
 
@@ -126,13 +42,17 @@
                 <div class="left-card-table"></div>
                 <div class="middle-card-table"></div>
                 <div class="right-card-table"></div>
-                <div class="card-container" style="background-image: url(graphics/JC.png);"></div>
+                <div class="card-container position-Player1" style="background-image: url(graphics/JC.png);">
+                    <div class="username-Player1">playernamegoeshere</div>    
+                </div>
+                <div class="card-container position-Player2" style="background-image: url(graphics/JS.png);"></div>
+                <div class="card-container position-Player3" style="background-image: url(graphics/JD.png);"></div>
+                <div class="card-container position-Player4" style="background-image: url(graphics/JH.png);"></div>
+                <div class="card-container position-Player5" style="background-image: url(graphics/AS.png);"></div>
+                <div class="card-container position-Player6" style="background-image: url(graphics/KS.png);"></div>
             </div>
         </div>
 
-        <!-- Le javascript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
         <script src="/js/jquery.js"></script>
         <script src="/js/bootstrap.js"></script>
 
