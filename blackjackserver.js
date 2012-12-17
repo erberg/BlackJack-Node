c5449d7b4b82cc0fafdb5f7f461d533f94544e48
erberg <berg.ericr@gmail.com>
2012-12-17 14:11:15 -0800

    De-Cluttered

--- blackjackserver.js
+++ blackjackserver.js
@@ -33,7 +33,6 @@ io.sockets.on('connection', function (socket) {
     socket.on('connect', function (data) {
         var cardout=board.publicCards.slice(0);
         socket.emit('updateTable', board);
-        socket.emit('tablePositions', board.tablePositions);
     });
     
   
